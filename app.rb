76bf58d9bd66c3a51e62c4a5c06377b2c02e6d6a
mthorry <mthorry@gmail.com>
2017-08-08 20:45:29 -0400

    done

--- app.rb
+++ app.rb
@@ -5,5 +5,40 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @new_pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @new_ships = Ship.all
+
+      erb :'pirates/show'
+    end
+
   end
 end
+
+# params[:student][:courses].each do |details|
+#     Course.new(details)
+
+# => params:
+    # {"pirate"=>
+    #   {"name"=>"Matt",
+    #    "weight"=>"150",
+    #    "height"=>"5'9\"",
+    #    "ships"=>
+    #     [{"name"=>"Santa Maria",
+    #       "type"=>"Sailboat",
+    #       "booty"=>"Big"},
+    #      {"name"=>"Cap'n",
+    #       "type"=>"Sailboat",
+    #       "booty"=>"Cash"}]}}
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,26 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  @@pirates = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@pirates << self
+  end
+
+  def self.all
+    @@pirates
+  end
+
+  def self.clear
+    @@pirates = []
+  end
 end
+
+# "pirate"=>
+    #   {"name"=>"Matt",
+    #    "weight"=>"150",
+    #    "height"=>"5'9\"",
+    #    "ships"=>
